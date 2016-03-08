9725d738def5fe70d920b87b5944efa02b13e7b5
Michelle Deng <mdeng@college.harvard.edu>
2016-03-07 23:12:12 -0500

    useful comments and planning

--- lclock.py
+++ lclock.py
@@ -3,23 +3,34 @@ import logging
 import socket
 import time
 
-logger = mp.log_to_stderr(logging.DEBUG)
 
-def worker(socket):
-    while True:
+TIME_TO_RUN_SECONDS = 60
+
+# wahoo
+def vm(sock1, sock2):
+
+    # file
+
+    # rand clock rate
+
+    # do the bullet points
+
+    while True: # math
         client, address = socket.accept()
         logger.debug("{u} connected".format(u=address))
         client.send("OK")
         client.close()
-        
+
 if __name__ == '__main__':
-    num_workers = 5
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock1.bind(('',9090))
+    sock1.listen(backlog=0)
 
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind(('',9090))
-    serversocket.listen(5)
+    serversocket.listen(0)
 
-    workers = [mp.Process(target=worker, args=(serversocket,)) for i in
+    workers = [mp.Process(target=worker, args=(socket1,socket2)) for i in
             range(num_workers)]
 
     for p in workers:
