b1c34a51acc3a9eebb19b44f7f119424fd34f5c8
Michelle Deng <mdeng@college.harvard.edu>
2016-03-08 02:57:54 -0500

    introduce sockets that work; define class VM; clean up code

--- lclock.py
+++ lclock.py
@@ -1,139 +1,190 @@
+#!/usr/bin/python
+
+import sys, socket, random, time, datetime
 import multiprocessing as mp
-import logging
-import socket
-import time
-import random
-import math
-import sys
-import datetime
 import Queue
 
 
 TIME_TO_RUN_SECONDS = 60
+PORTS = [8080, 8081, 8082]
 
-def send_message(lc, sock):
+class VM:
+    def __init__(self, name, server_sock, client_sock, server_conn):
+        self.name = name
+        self.ss = server_sock
+        self.cs = client_sock
+        self.sc = server_conn
 
-# int - no message available is -1 
-def receive_message(sock, queue):
+        self.logfile = open(name + '.out', 'w')
 
-def inc_time(i, f, sleep_time, end_time): 
-    time.sleep(sleep_time)
-    if (i + 1) == end_time:
-        f.close()
+        # rand clock rate
+        self.num_actions = random.randint(1,6)
+        self.sleep_time = 1./num_actions
+
+        # initialize LC
+        self.lc = 0
+
+        # initialize message queue
+        self.msg_q = Queue.Queue()
+
+        # i
+        self.ticks = 0
+        self.end_time = num_actions * TIME_TO_RUN_SECONDS
+
+    def shutdown(self):
+        self.ss.close()
+        self.cs.close()
+        self.logfile.close()
         sys.exit()
-    return i + 1
 
-def process_rec(rec_message, proc_name, i, lc, sleep_time, \
-                                 end_time, queue_len, f):
-    lc = max(lc + 1, rec_message + 1)
-    i = inc_time(i, f, sleep_time, end_time)
-    global_time = str(datetime.datetime.now())
-    f.write("Received message from process " + proc_name + \
-            ". \nNum messages queued: " + \
-            queue_len + "\nLogical clock: " + str(lc) + \
-            "\nSystem time: " + global_time + "\n\n")
-    i = inc_time(i, f, sleep_time, end_time)
-    return (i, lc)
+    def inc_ticks(self):
+        time.sleep(sleep_time)
+        self.ticks += 1
+        if self.ticks == self.end_time:
+            self.shutdown()
 
-def process_send(conn, proc_name, i, lc, sleep_time, end_time, f):
-    send_message(lc, sock)
-    i = inc_time(i, f, sleep_time, end_time)
-    lc += 1
-    i = inc_time(i, f, sleep_time, end_time)
-    return (i, lc)
+    ###### Tick-sized actions
 
-def process_send_one(conn, proc_name, i, lc, sleep_time, end_time, f):
-    (i, lc) = process_send(conn, proc_name, i, lc, sleep_time, end_time, f)
-    f.write("Sent message to process " + proc_name + \
-            ". \nLogical clock: " + str(lc) + \
-            "\nSystem time: " + global_time + "\n\n")
-    i = inc_time(i, f, sleep_time, end_time)
-    return (i, lc)
+    def inc_lc(self):
+        self.lc += 1
+        self.inc_ticks()
 
-def process_send_both(conns, proc_names, i, lc, sleep_time, end_time, f):
-    (i, lc) = process_send(conns[0], proc_names[0], i, lc, \
-                                        sleep_time, end_time, f)
-    (i, lc) = process_send(conns[1], proc_names[1], i, lc, \
-                                        sleep_time, end_time, f)
-    f.write("Sent message to processes " + proc_names[0] + \
-            " and " + proc_names[1] + \
-            ". \nLogical clock: " + str(lc) + \
-            "\nSystem time: " + global_time + "\n\n")
-    i = inc_time(i, f, sleep_time, end_time)
-    return (i, lc)
+    def sync_lc(self, other_lc):
+        self.lc = max(self.lc + 1, other_lc)
+        self.inc_ticks()
 
-# wahoo
-def vm(name, sock1, sock2, name1, name2):
-    conn = []
+    def generate_action_type(self):
+        action_type = random.randint(0, 9)
+        self.inc_ticks() # TODO: remove this?
+        return action_type
 
-    # file
-    f = open(name + '.out', 'w')
+    def log_send(self):
+        # TODO: add process name (or port)
+        self.logfile.write( \
+            "[Sent message] \n" \
+            "Logical clock: %d \n "\
+            "System time: %s\n" \
+            % (self.lc, datetime.datetime.now()))
+        self.inc_ticks()
 
-    # rand clock rate
-    num_actions = math.floor(random.random() * 6) + 1
-    sleep_time = 1./num_actions
+    def log_recv(self, other_name):
+        self.logfile.write( \
+            "[Received message] \n" \
+            "Sender: %s" \
+            "Num messages queued: %d" \
+            "Logical clock: %d \n "\
+            "System time: %s\n" \
+            % (other_name, len(self.msg_q), self.lc, datetime.datetime.now()))
+        self.inc_ticks()
 
-    # initialize LC
-    lc = 0
+    def log_internal(self):
+        self.logfile.write( \
+            "[Internal event] \n" \
+            "Logical clock: %d \n "\
+            "System time: %s\n" \
+            % (self.lc, datetime.datetime.now()))
+        self.inc_ticks()
 
-    # initialize message queue
-    queue = Queue.Queue()
 
-    # do the bullet points
-    end_time = num_actions * TIME_TO_RUN_SECONDS
-    i = 0
-    while i < end_time: # math
-        queue_len = len(queue)
-        (rec_message, proc_name) = receive_message(conn[0], queue)
-        i = inc_time(i, f, sleep_time, end_time)
-        if (rec_message != -1):
-            (i, lc) = process_rec(rec_message, proc_name, i, lc, \
-                             sleep_time, end_time, queue_len, f)
-            continue
-        (rec_message, proc_name) = receive_message(conn[1], queue)
-        i = inc_time(i, f, sleep_time, end_time)
-        if (rec_message != -1):
-            (i, lc) = process_rec(rec_message, proc_name, i, lc, \
-                            sleep_time, end_time, queue_len, f)
-            continue
-        action_type = math.floor(random.random() * 10)
+    ######
+
+    def pop_msg(self, sock):
+        # TODO 
+        # return msg, vm name
+        return None
+
+    def process_send(self, sock):
+        self.send_message(sock)
+        self.inc_lc()
+        self.log_send()
+
+    def process_msg(msg):
+        (other_lc, other_name) = msg
+        self.sync_lc(other_lc)
+        self.log_recv()
+
+    #####
+
+    def run_cycle(self):
+        # TODO: vary order of checking conn0/1
+
+        # check for messages
+        msg = self.pop_msg(self.sc)
+        if msg:
+            self.process_msg(msg)
+            return
+        
+        # check second socket for messages
+        # i don't know if we actually need this
+        #msg, vm_name = self.receive_message(self.sc)
+        #if msg:
+        #    self.process_msg(msg, vm_name)
+        #    return
+
+        action_type = self.generate_action_type()
+        
         if (action_type == 0):
-            (i, lc) = process_send_one(conn[0], name1, i, lc, \
-                            sleep_time, end_time, f)
-        elif (action_type == 1):
-            (i, lc) = process_send_one(conn[1], name2, i, lc, \
-                            sleep_time, end_time, f)
-        elif (action_type == 2):
-            (i, lc) = process_send_both(conn, [name1, name2], i, lc, \
-                            sleep_time, end_time, f)
-        else:
-            lc += 1
-            i = inc_time(i, f, sleep_time, end_time)
-            f.write("Internal event. \nLogical clock: " + str(lc) + \
-                    "\nSystem time: " + global_time + "\n\n")
-            i = inc_time(i, f, sleep_time, end_time)
+            self.process_send(self.sc)
 
-    f.close()
-    client.close()
+        elif (action_type == 1):
+            self.process_send(self.cs)
+
+        elif (action_type == 2):
+            self.process_send(self.sc)
+            self.process_send(self.cs)
+
+        else:
+            self.inc_lc()
+            self.log_internal()
+
+    def run(self):
+        while self.ticks < self.end_time:
+            self.run_cycle()
+
+
+#### MAIN
+
+def init_server_socket(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((socket.gethostname(), port))
+    sock.listen(5)
+    return sock
+
+def connect_client_socket(port):
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((socket.gethostname(), port))
+        if result:
+            print result
+            sock.close()
+        else:
+            return sock
+
+def vm_main(name, ports):
+    ss = init_server_socket(ports[0]) 
+    cs = connect_client_socket(ports[1])   
+    sc, _ = s.accept() # will block until other VM connects
+
+    vm = VM(name, ss, cs, sc)
+    vm.run()
+
+    #c.send(name)
+    #str = ss.recv(4096)
+    #print str
+
+    #s.close()
+    #c.close()
+
+def launch_vm(name, ports):
+    vm = mp.Process(target=vm_main, args=(name, ports))
+    vm.start()
+    return vm
 
 if __name__ == '__main__':
-    sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock1.bind(('',9090))
-    sock1.listen(backlog=0)
+    vm1 = launch_vm("vm1", (PORTS[0], PORTS[1]))
+    vm2 = launch_vm("vm2", (PORTS[1], PORTS[2]))
+    vm3 = launch_vm("vm3", (PORTS[2], PORTS[0]))
 
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('',9090))
-    serversocket.listen(0)
-
-    workers = [mp.Process(target=worker, args=(socket1,socket2)) for i in
-            range(num_workers)]
-
-    for p in workers:
-        p.daemon = True
-        p.start()
-
-    while True:
-        try:
-            time.sleep(10)
-        except:
-            break
+    vm1.join()
+    vm2.join()
+    vm3.join()
