92aa5a6833b08bac2fab4419030bde9d1962baa5
Michelle Deng <mdeng@college.harvard.edu>
2016-03-19 17:24:20 -0400

    indentation

--- vm.h
+++ vm.h
@@ -11,13 +11,13 @@ typedef int bool;
 #define false 0
 
 /* Runtime parameters */
-#define MAX_TICKS_PER_SECOND 6		// maximum number of instructions that a VM 
-									// may execute per second; each VM chooses
-									// a limit randomly between 1 and this max
-#define MAX_ACTION_CHOICES 15		// number of types of actions that can be 
-									// simulated per cycle
-#define NUM_VMS 3					// number of VMs in the simulation
-#define LOG_EXTENSION ".out"		// extension for logfiles
+#define MAX_TICKS_PER_SECOND 6      // maximum number of instructions that a VM 
+                                    // may execute per second; each VM chooses
+                                    // a limit randomly between 1 and this max
+#define MAX_ACTION_CHOICES 15       // number of types of actions that can be 
+                                    // simulated per cycle
+#define NUM_VMS 3                   // number of VMs in the simulation
+#define LOG_EXTENSION ".out"        // extension for logfiles
 
 /* 
  * This file defines `struct vm`, our representation of a VM. `vm.h` exposes
@@ -33,58 +33,58 @@ typedef int bool;
  */
 
 struct vm {
-	/* This VM's ID. Must be unique from the other VMs for the simulation
-	 * to function properly. */
-	int id;
+    /* This VM's ID. Must be unique from the other VMs for the simulation
+     * to function properly. */
+    int id;
 
-	/* Serving socket, where this VM receives all messages from other VMs */
-	int srv_sock;
-	/* Client connections to other VM's server sockets; this VM sends messages 
-	 * to other clients via these sockets */
-	int cli_sock[NUM_VMS - 1];
-	/* IDs of VMs corresponding to cli_sock above */
-	int cli_ids[NUM_VMS - 1]; 
+    /* Serving socket, where this VM receives all messages from other VMs */
+    int srv_sock;
+    /* Client connections to other VM's server sockets; this VM sends messages 
+     * to other clients via these sockets */
+    int cli_sock[NUM_VMS - 1];
+    /* IDs of VMs corresponding to cli_sock above */
+    int cli_ids[NUM_VMS - 1]; 
 
-	/* This VM's log file, named [id].out */
-	FILE *logfile;
+    /* This VM's log file, named [id].out */
+    FILE *logfile;
 
-	/* This VM's logical clock */
-	int lc;
+    /* This VM's logical clock */
+    int lc;
 
-	/* A 'tick' equals one tick of this VM's simulated system clock; that is, 
-	 * one instruction can execute per tick. `ticks` is the VM's tick-counter, and
-	 * the system exits after `end_tick` ticks have elapsed. After each 
-	 * instruction, we increment `ticks`.
-	 * 
-	 * So, if `sec_to_run` is 10, and we want to operate at 2 instructions per 
-	 * second, then `end_tick` would be 20.
-	 */
-	int ticks;
-	int end_tick;
+    /* A 'tick' equals one tick of this VM's simulated system clock; that is, 
+     * one instruction can execute per tick. `ticks` is the VM's tick-counter, and
+     * the system exits after `end_tick` ticks have elapsed. After each 
+     * instruction, we increment `ticks`.
+     * 
+     * So, if `sec_to_run` is 10, and we want to operate at 2 instructions per 
+     * second, then `end_tick` would be 20.
+     */
+    int ticks;
+    int end_tick;
 
-	/* Time (in seconds) that this VM sleeps between instructions/ticks 
-	 * to maintain its simulated clock rate */
-	float sleep_time;
+    /* Time (in seconds) that this VM sleeps between instructions/ticks 
+     * to maintain its simulated clock rate */
+    float sleep_time;
 
-	/* VM's message queue, a FIFO linked list of messages. */
-	struct message *msg_head; // newest
-	struct message *msg_tail; // oldest
-	int msg_count;
-	/* Protects access of queue & its count between background socket daemon 
-	 * and main VM thread */
-	pthread_mutex_t msg_lock;
+    /* VM's message queue, a FIFO linked list of messages. */
+    struct message *msg_head; // newest
+    struct message *msg_tail; // oldest
+    int msg_count;
+    /* Protects access of queue & its count between background socket daemon 
+     * and main VM thread */
+    pthread_mutex_t msg_lock;
 };
 
 /* 
  * Arguments `struct vm` needs for initialization.
  */
 struct vm_args {
-	/* This VM's ID. */
-	int id;
-	/* IDs of all VMs in the system. */
-	int *all_ids; 
-	/* Seconds the simulation should run. */
-	int sec_to_run;
+    /* This VM's ID. */
+    int id;
+    /* IDs of all VMs in the system. */
+    int *all_ids; 
+    /* Seconds the simulation should run. */
+    int sec_to_run;
 };
 
 /* 
